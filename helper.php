753acb5ca20d4984192ba48811b7d6d9e8189742
Bear <programming@hallhome.us>
2025-05-13 08:17:40 -0500

    Remove load_stylesheet parameter and update language constants

--- helper.php
+++ helper.php
@@ -36,31 +36,26 @@ class ModBearsPricingTablesHelper
         // Get template selection
         $template = (int) $params->get('bears_template', 1);
         
-        // Check if we should load CSS
-        $loadCSS = (int) $params->get('load_stylesheet', 1);
+        // Map template values to CSS files
+        $templateFiles = [
+            1 => 'default.css',    // 1 - Default ( Large Accent )
+            2 => 'blue.css',       // 2 - Blue ( Rounded )
+            3 => 'dark.css',       // 3 - Dark ( Small Accent )
+            4 => 'purple.css',     // 4 - Purple ( Wedge Accent )
+            5 => 'orange.css',     // 5 - Orange ( With Icon )
+            6 => 'coins.css',      // 6 - Coins ( Blue )
+            7 => 'offset.css',     // 7 - Offset ( Accent color )
+            8 => 'burgundy.css',   // 8 - Burgundy ( Coin Pricing )
+            9 => 'simple.css',     // 9 - Simple ( Blue on white )
+            10 => 'white.css',     // 10 - White ( Black and White )
+            11 => 'small.css'      // 11 - Small ( Small Title )
+        ];
         
-        if ($loadCSS) {
-            // Map template values to CSS files
-            $templateFiles = [
-                1 => 'default.css',    // 1 - Default ( Large Accent )
-                2 => 'blue.css',       // 2 - Blue ( Rounded )
-                3 => 'dark.css',       // 3 - Dark ( Small Accent )
-                4 => 'purple.css',     // 4 - Purple ( Wedge Accent )
-                5 => 'orange.css',     // 5 - Orange ( With Icon )
-                6 => 'coins.css',      // 6 - Coins ( Blue )
-                7 => 'offset.css',     // 7 - Offset ( Accent color )
-                8 => 'burgundy.css',   // 8 - Burgundy ( Coin Pricing )
-                9 => 'simple.css',     // 9 - Simple ( Blue on white )
-                10 => 'white.css',     // 10 - White ( Black and White )
-                11 => 'small.css'      // 11 - Small ( Small Title )
-            ];
-            
-            // Get the CSS file for the selected template
-            $cssFile = isset($templateFiles[$template]) ? $templateFiles[$template] : 'default.css';
-            
-            // Add the CSS file to the document
-            $document->addStyleSheet(Uri::base() . 'modules/mod_bears_pricing_tables/css/' . $cssFile);
-        }
+        // Get the CSS file for the selected template
+        $cssFile = isset($templateFiles[$template]) ? $templateFiles[$template] : 'default.css';
+        
+        // Add the CSS file to the document
+        $document->addStyleSheet(Uri::base() . 'modules/mod_bears_pricing_tables/css/' . $cssFile);
     }
     
     /**
